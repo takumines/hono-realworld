cd7f9c631b8fc7a498e356864fdba95cc71dc81c
takumines <takumines1@gmail.com>
2024-09-29 18:27:02 +0900

    [add] レスポンスを扱うファイルを作成

---
+++ src/interfaces/http/response/response.ts
@@ -0,0 +1,13 @@
+const baseHeaders = {
+	'Content-Type': 'application/json',
+}
+
+/**
+ * ステータスコード200のレスポンスを返す
+ */
+export const okResponse = (result: BodyInit) => {
+	return new Response(result, {
+		status: 200,
+		headers: baseHeaders,
+	});
+};
