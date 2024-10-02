cd2df3e43407b486da2b684e21487b5e8e76eb22
takumines <takumines1@gmail.com>
2024-10-02 23:41:02 +0900

    [add] migration実行

--- prisma/migrations/20241001095335_/migration.sql
+++ prisma/migrations/20241001095335_/migration.sql
@@ -3,7 +3,9 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "image" TEXT DEFAULT 'https://api.realworld.io/images/smiley-cyrus.jpeg',
+    "bio" TEXT,
     "emailVerifiedAt" TIMESTAMP(3),
     "verificationCode" CHAR(6),
     "verificationCodeExpiresAt" TIMESTAMP(3) NOT NULL,
@@ -15,6 +17,9 @@ CREATE TABLE "User" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_verificationCode_key" ON "User"("verificationCode");
